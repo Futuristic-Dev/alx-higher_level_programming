e2516b715329657228fb0b8880c8be24e5d9483b
Von-sledge <papaaamoah568@gmail.com>
2023-12-20 04:29:58 +0000

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-""" Creating an empty class Square that  define a square:"""
+"""Creating an empty class Square that  define a square."""
 
 class Square:
+    """This class has been imported into another module."""
+
     pass
