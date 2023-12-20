e803f63d9fe3cbafc7baa3165b8af8d8c6343c16
Von-sledge <papaaamoah568@gmail.com>
2023-12-20 04:43:40 +0000

    Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 
-"""A class that defines a square based on 0-square.py"""
+"""A class that defines a square based on 0-square.py."""
+"""with a private instance attribute, size."""
+
+
+class Square:
+    """Defining the class:Square."""
+
+    def __init__(self, size):
+        """Initialise the private instance attribute."""
+        self.__size = size
+
+
 
