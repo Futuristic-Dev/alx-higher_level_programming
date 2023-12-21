e32b9fd591f43c2fc9383e35b2a99b9eb8bdd1d0
Von-sledge <papaaamoah568@gmail.com>
2023-12-21 05:54:28 +0000

    Python Exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Create a function that prints a list of integers."""
 
-def Safe_print_list_integers(my_list=[], x=0):
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Create a functions that creates a list of integers."""
     try:
         count = 0
         for i in range(x):
