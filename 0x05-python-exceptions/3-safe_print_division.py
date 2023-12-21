0ef357d301a2cb69cbfdd3122556c3f4c8025d60
Von-sledge <papaaamoah568@gmail.com>
2023-12-21 05:58:35 +0000

    Python Exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,13 @@
 
 #!/usr/bin/python3
+"""Create a functions for division."""
+
 
 def safe_print_division(a, b):
+    """Create a function for division."""
     result = None
 
-    Try:
+    try:
         result = a / b
     except ZeroDivisionError:
         result = None
