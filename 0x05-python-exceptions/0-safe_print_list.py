cf146244567ef03d4177c7e07e97e351386d2902
Von-sledge <papaaamoah568@gmail.com>
2023-12-21 05:40:43 +0000

    Python Excpetions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list = [], x = 0):
+"""Create a function that prints integers."""
+
+def safe_print_list(my_list=[], x=0):
+    """Create a function that prints integers."""
     nb_print = 0
     try:
         for i in range(x):
@@ -9,5 +12,5 @@ def safe_print_list(my_list = [], x = 0):
     except IndexError:
         pass
     finally:
-        print(" ")
+        print("")
         return nb_print
