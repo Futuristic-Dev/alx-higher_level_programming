925acc5629eefdfad8f1cceb26ee69776b9407fb
Von-sledge <papaaamoah568@gmail.com>
2023-12-21 17:19:15 +0000

    Data Sructures: List and Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Create a function that prints all integers of a list."""
 
 
 def print_list_integer(my_list=[]):
+    """Create function to print the integers."""
     for item in my_list:
         print("{:d}".format(item))
-
