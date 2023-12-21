19e37351713b1685d18fe5c7a4b75cd1ad7ffe09
Von-sledge <papaaamoah568@gmail.com>
2023-12-21 05:25:29 +0000

    Pytho Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_safe_print_list(my_list = [], x = 0):
+def safe_print_list(my_list = [], x = 0):
     nb_print = 0
     try:
         for i in range(x):
@@ -9,5 +9,5 @@ def safe_print_safe_print_list(my_list = [], x = 0):
     except IndexError:
         pass
     finally:
-        print()
+        print(" ")
         return nb_print
