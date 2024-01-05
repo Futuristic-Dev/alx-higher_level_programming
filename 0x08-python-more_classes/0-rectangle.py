47a1775f9fdb836d6dbc17b618045b5e27ee407b
Von-sledge <papaamoah568@gmail.com>
2024-01-05 01:47:44 +0000

    More classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+    """
+    Write a class that defines a Rectangle.
+    Empty class. Pass the ball to the next player.
+    """
+
+    class Rectangle:
+        """An empty class function.
+        """
+
+        pass
