84ccc7d3af537e02d0c024e71ee22515da3347e9
Von-sledge <papaamoah568@gmail.com>
2024-01-10 02:24:31 +0000

    Python inputs/outputs

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Log Parsing Module."""
+
+
+
+import sys
+
+metric = {
+        'total_size': 0,
+        'status_codes': {200: 0, 301: 0, 400: 0, 403: 0,
+                        404: 0, 405: 0, 500: 0}
+        'line_count': 0
+        }
+
+try: 
+    for ine in sys.stdin:
+        try:
+            parts = line.split()
+            if len(parts) >= 7:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
+                metrics['total_size'] += file_size
+                if status_code in metrics['status_codes']:
+                    metrics['status_codes'][status_code] += 1
+                    metric['line_count'] += 1
+
+                    if metrics['line_count'] % 10 == 0:
+                        print("File size: {}".format(metrics['total_size']))
+                        for code in sorted(metrics['status_codes']):
+                            if metrics['status_codes'][code] > 0:
+                                print("{}: {}".format(
+                                    code, metrics['status_codes'][code]
+                                ))
+
+        except ValueError:
+            continue
+        except KeyboardInterrupt:
+            pass
+
+        print("File size: {}".format(metrics['total_size']))
+        for code in sorted(metrics['status_codes']):
+            if metrics['status_codes'][code] > 0:
+                print("{}: {}".format(code, metrics['status_codes'][code]))
