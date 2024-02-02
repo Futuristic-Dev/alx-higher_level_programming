da0fc6613e37cdfebee84504929720ace827b9c0
Von-sledge <papaamoah568@gmail.com>
2024-02-02 17:10:26 +0000

    New commit

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str_value = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+substring = str_value[39:67] + str_value[107:112] + str_value[:6]
+print(substring)
