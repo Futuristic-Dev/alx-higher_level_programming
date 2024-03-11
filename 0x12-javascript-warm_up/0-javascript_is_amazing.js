d0b0d7baed128a5324add3b516804c026fadeffd
Von-sledge <papaamoah568@gmail.com>
2024-03-11 16:33:09 +0000

    Task 0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-
-const myVar = ' JavaScript is amazing ' // Print "JavaScript is amazing"
+// Print "JavaScript is amazing"
+const myVar = 'JavaScript is amazing'
 console.log(myVar)
