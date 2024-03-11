3731eb4d68f3a26d772a5faf5bf8ced54ebca340
Von-sledge <papaamoah568@gmail.com>
2024-03-11 16:53:23 +0000

    Add Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Prints 3 lines
+const myVar1 = 'C is fun';
+console.log(myVar1);
+const myVar2 = 'Python is cool';
+console.log(myVar2);
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar3);
