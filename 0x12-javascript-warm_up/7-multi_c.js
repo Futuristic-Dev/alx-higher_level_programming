545615c1be82ed0925f9a054ee98de55f4a92370
Von-sledge <papaamoah568@gmail.com>
2024-03-14 05:52:11 +0000

    add Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const numVar = parseInt(process.arg[2]);
+const numVar = parseInt(process.argv[2]);
 if (isNaN(numVar)) {
-  console.log('Missing number of instances');
+  console.log('Missing number of occurences');
 } else {
   let i;
   for (i = 0; i < numVar; i++) {
