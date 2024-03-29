95d2fd9f6a8d2ca55e87ace36a910bd5cca10b60
Von-sledge <papaamoah568@gmail.com>
2024-03-29 06:06:33 +0000

    Add Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,9 +14,9 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    e-mail = sys.argv[2]
+    email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'e-mail': email})
+    data = urllib.parse.urlencode({'email': email})
     data = data.encode('utf-8')
 
     req = urllib.request.Request(url, data=data, method='POST')
