8fefb483515b3a3df945eafe32dda384f3ed28a2
Von-sledge <papaamoah568@gmail.com>
2024-04-01 05:32:45 +0000

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,10 +14,6 @@ import urllib.request
 
 
 if __name__ == "__main__":
-     if len(sys.argv) < 3:
-        print("Usage: python script.py <URL> <email>")
-        sys.exit(1)
-
         url = sys.argv[1]
         email = sys.argv[2]
 
